ca539a733c1645f45c9df5584c59ea77d6a2bece
gbnijnugyf <1612517397@qq.com>
2023-07-25 23:13:00 +0800

    lesson1-2

---
+++ lesson1-2.go
@@ -0,0 +1,257 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"encoding/binary"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+)
+
+const socks5Ver = 0x05
+const cmdBind = 0x01
+const atypeIPV4 = 0x01
+const atypeHost = 0x03
+const atypeIPV6 = 0x04
+
+type DictResponse struct {
+	Rc   int `json:"rc"`
+	Wiki struct {
+	} `json:"wiki"`
+	Dictionary struct {
+		Prons struct {
+			EnUs string `json:"en-us"`
+			En   string `json:"en"`
+		} `json:"prons"`
+		Explanations []string      `json:"explanations"`
+		Synonym      []string      `json:"synonym"`
+		Antonym      []string      `json:"antonym"`
+		WqxExample   [][]string    `json:"wqx_example"`
+		Entry        string        `json:"entry"`
+		Type         string        `json:"type"`
+		Related      []interface{} `json:"related"`
+		Source       string        `json:"source"`
+	} `json:"dictionary"`
+}
+
+type DictRequest struct {
+	TransType string `json:"trans_type"`
+	Source    string `json:"source"`
+	UserID    string `json:"user_id"`
+}
+
+func setHttpPost(request DictRequest) {
+
+	buf, err := json.Marshal(request)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	//创建http Client
+	client := &http.Client{}
+
+	//创建数据流
+	//var data = strings.NewReader(`{"trans_type":"en2zh","source":"good"}`)
+	var data = bytes.NewReader(buf)
+
+	//创建http post请求
+	req, err := http.NewRequest("POST", "https://api.interpreter.caiyunai.com/v1/dict", data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("authority", "api.interpreter.caiyunai.com")
+	req.Header.Set("accept", "application/json, text/plain, */*")
+	req.Header.Set("accept-language", "zh-CN,zh;q=0.9")
+	req.Header.Set("app-name", "xy")
+	req.Header.Set("content-type", "application/json;charset=UTF-8")
+	req.Header.Set("device-id", "d7c58582765d6927f6cf939834e65d70")
+	req.Header.Set("origin", "https://fanyi.caiyunapp.com")
+	req.Header.Set("os-type", "web")
+	req.Header.Set("os-version", "")
+	req.Header.Set("referer", "https://fanyi.caiyunapp.com/")
+	req.Header.Set("sec-ch-ua", "^\\^Not.A/Brand^^;v=^\\^8^^, ^\\^Chromium^^;v=^\\^114^^, ^\\^Google")
+	req.Header.Set("sec-ch-ua-mobile", "?0")
+	req.Header.Set("sec-ch-ua-platform", "^\\^Windows^^")
+	req.Header.Set("sec-fetch-dest", "empty")
+	req.Header.Set("sec-fetch-mode", "cors")
+	req.Header.Set("sec-fetch-site", "cross-site")
+	req.Header.Set("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
+	req.Header.Set("x-authorization", "token:qgemv4jr1y38jyq6vhvi")
+
+	//发送请求
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	bodyText, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if resp.StatusCode != 200 {
+		log.Fatal("bad StatusCode", resp.StatusCode, "body", string(bodyText))
+	}
+
+	var dictResponse DictResponse
+	err = json.Unmarshal(bodyText, &dictResponse)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//fmt.Printf("%#v\n", dictResponse)
+	fmt.Println(request.Source, "UK:", dictResponse.Dictionary.Prons.En, "US:", dictResponse.Dictionary.Prons.EnUs)
+	for _, item := range dictResponse.Dictionary.Explanations {
+		fmt.Println(item)
+	}
+}
+
+func Dictionary() {
+	var word string
+
+	for {
+		fmt.Println("英语单词字典：")
+		fmt.Scanln(&word)
+		request := DictRequest{TransType: "en2zh", Source: word}
+		setHttpPost(request)
+	}
+
+}
+
+func TCPEchoServer() {
+	server, err := net.Listen("tcp", "127.0.0.1:1080")
+	if err != nil {
+		panic(err)
+	}
+	for {
+		client, err := server.Accept()
+		if err != nil {
+			log.Printf("Accept fialed %v", err)
+			continue
+		}
+		go process(client)
+	}
+}
+
+func auth(reader *bufio.Reader, conn net.Conn) (err error) {
+
+	ver, err := reader.ReadByte()
+	if err != nil {
+		return fmt.Errorf("read ver failed:%w", err)
+	}
+	if ver != socks5Ver {
+		return fmt.Errorf("not support ver: %v", ver)
+	}
+	methodSize, err := reader.ReadByte()
+	if err != nil {
+		return fmt.Errorf("read methodSize failed:%w", err)
+	}
+	method := make([]byte, methodSize)
+	_, err = io.ReadFull(reader, method)
+	if err != nil {
+		return fmt.Errorf("read method failed; %w", err)
+	}
+	//log.Println("ver", ver, "method", method)
+	_, err = conn.Write([]byte{socks5Ver, 0x00})
+	if err != nil {
+		return fmt.Errorf("write failed:%w", err)
+	}
+	return nil
+}
+
+func connect(reader *bufio.Reader, conn net.Conn) (err error) {
+	buf := make([]byte, 4)
+	_, err = io.ReadFull(reader, buf)
+	if err != nil {
+		return fmt.Errorf("reader header failed:%w", err)
+	}
+	ver, cmd, atype := buf[0], buf[1], buf[3]
+	if ver != socks5Ver {
+		return fmt.Errorf("not supported ver:%v", ver)
+	}
+	if cmd != cmdBind {
+		return fmt.Errorf("not support cmd:%v", cmd)
+	}
+	addr := ""
+	switch atype {
+	case atypeIPV4:
+		_, err = io.ReadFull(reader, buf)
+		if err != nil {
+			return fmt.Errorf("read atype failed:%w", err)
+		}
+		addr = fmt.Sprintf("%d.%d.%d.%d", buf[0], buf[1], buf[2], buf[3])
+	case atypeHost:
+		hostSize, err := reader.ReadByte()
+		if err != nil {
+			return fmt.Errorf("read hostSize failed:%w", err)
+		}
+		host := make([]byte, hostSize)
+		_, err = io.ReadFull(reader, host)
+		if err != nil {
+			return fmt.Errorf("read hostSize failed:%w", err)
+		}
+		addr = string(host)
+	case atypeIPV6:
+		return errors.New("IPV6: no supported yet")
+	default:
+		return errors.New("invalid atype")
+	}
+	_, err = io.ReadFull(reader, buf[:2])
+	if err != nil {
+		return fmt.Errorf("read port failed:%w", err)
+	}
+	port := binary.BigEndian.Uint16(buf[:2])
+
+	dest, err := net.Dial("tcp", fmt.Sprintf("%v:%v", addr, port))
+	if err != nil {
+		return fmt.Errorf("dial dst failed:%w", err)
+	}
+	defer dest.Close()
+	log.Println("dial", addr, port)
+	_, err = conn.Write([]byte{0x05, 0x00, 0x00, 0x01, 0, 0, 0, 0, 0, 0})
+	if err != nil {
+		return fmt.Errorf("write failed: %w", err)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() {
+		_, _ = io.Copy(dest, reader)
+		cancel()
+	}()
+	go func() {
+		_, _ = io.Copy(conn, dest)
+		cancel()
+	}()
+
+	<-ctx.Done()
+	return nil
+}
+
+func process(conn net.Conn) {
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+	err := auth(reader, conn)
+	if err != nil {
+		log.Printf("client %v auth failed:%v", conn.RemoteAddr(), err)
+		return
+	}
+	//log.Println("auth success")
+	err = connect(reader, conn)
+	if err != nil {
+		log.Printf("client %v auth failed:%v", conn.RemoteAddr(), err)
+		return
+	}
+}
+
+func main() {
+	TCPEchoServer()
+}
